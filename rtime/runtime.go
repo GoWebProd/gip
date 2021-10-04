a6d52ea18ba9d6bd280590619be08424d6de8065
Alexandr Mayorskiy <a.mayorskiy@corp.mail.ru>
2021-10-04 22:31:49 +0300

    add gopack/goready

--- rtime/runtime.go
+++ rtime/runtime.go
@@ -1,7 +1,7 @@
 package rtime
 
 import (
-	_ "unsafe"
+	"unsafe"
 )
 
 //go:linkname ProcPin runtime.procPin
@@ -9,3 +9,9 @@ func ProcPin() int
 
 //go:linkname ProcUnpin runtime.procUnpin
 func ProcUnpin()
+
+//go:linkname GoPark runtime.gopark
+func GoPark(unlockf func(unsafe.Pointer, unsafe.Pointer) bool, lock unsafe.Pointer, reason uint8, traceEv byte, traceskip int)
+
+//go:linkname GoReady runtime.goready
+func GoReady(gp unsafe.Pointer, traceskip int)
