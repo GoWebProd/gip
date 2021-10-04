3ddd41d029c0cf8a090e666ddff300d0a59a9018
Alexandr Mayorskiy <a.mayorskiy@corp.mail.ru>
2021-10-04 23:49:09 +0300

    add fasttime package

---
+++ fasttime/fasttime.go
@@ -0,0 +1,146 @@
+package fasttime
+
+import (
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+)
+
+const (
+	laepoch = 946684800 + 86400*(31+29)
+
+	daysPer400y = 365*400 + 97
+	daysPer100y = 365*100 + 24
+	daysPer4y   = 365*4 + 1
+)
+
+var daysInMonth = [...]int64{31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 31, 29}
+
+/* Originally released by the musl project (http://www.musl-libc.org/) under the
+ * MIT license. Taken from the file /src/time/__secs_to_tm.c */
+func ParseDateTime(timestamp int64) (year int64, month int64, day int64, hour int64, minute int64, seconds int64) {
+	secs := timestamp - laepoch
+	days := secs / 86400
+
+	remsecs := secs % 86400
+	if remsecs < 0 {
+		remsecs += 86400
+		days--
+	}
+
+	wday := (3 + days) % 7
+	if wday < 0 {
+		wday += 7
+	}
+
+	qcCycles := days / daysPer400y
+	remdays := days % daysPer400y
+	if remdays < 0 {
+		remdays += daysPer400y
+		qcCycles--
+	}
+
+	cCycles := remdays / daysPer100y
+	if cCycles == 4 {
+		cCycles--
+	}
+	remdays -= cCycles * daysPer100y
+
+	qCycles := remdays / daysPer4y
+	if qCycles == 25 {
+		qCycles--
+	}
+	remdays -= qCycles * daysPer4y
+
+	remyears := remdays / 365
+	if remyears == 4 {
+		remyears--
+	}
+	remdays -= remyears * 365
+
+	var leap int64
+	if remyears == 0 && (qCycles > 0 || cCycles == 0) {
+		leap = 1
+	}
+
+	yday := remdays + 31 + 28 + leap
+	if yday >= 365+leap {
+		yday -= 365 + leap
+	}
+
+	years := remyears + 4*qCycles + 100*cCycles + 400*qcCycles
+
+	var months int64
+	for months = 0; daysInMonth[months] <= remdays; months++ {
+		remdays -= daysInMonth[months]
+	}
+
+	tmYear := years + 2000
+	tmMon := months + 2
+	if tmMon >= 12 {
+		tmMon -= 12
+		tmYear++
+	}
+	tmMday := remdays + 1
+
+	tmHour := remsecs / 3600
+	tmMin := remsecs / 60 % 60
+	tmSec := remsecs % 60
+
+	return tmYear, tmMon, tmMday, tmHour, tmMin, tmSec
+}
+
+func ParseDate(timestamp int64) (year int64, month int64, day int64) {
+	year, month, day, _, _, _ = ParseDateTime(timestamp)
+	return
+}
+
+func FormatTimestampToDate(timestamp int64) string {
+	return FormatDate(ParseDate(timestamp))
+}
+
+func FormatDate(year int64, month int64, day int64) string {
+	month += 1
+
+	sb := strings.Builder{}
+	sb.WriteString(strconv.Itoa(int(year)))
+	if month < 10 {
+		sb.WriteString("0")
+	}
+	sb.WriteString(strconv.Itoa(int(month)))
+	if day < 10 {
+		sb.WriteString("0")
+	}
+	sb.WriteString(strconv.Itoa(int(day)))
+
+	return sb.String()
+}
+
+func Now() int64 {
+	var tv syscall.Timeval
+	err := syscall.Gettimeofday(&tv)
+	if err != nil {
+		return time.Now().Unix()
+	}
+	return tv.Sec
+}
+
+func NowNano() int64 {
+	var tv syscall.Timeval
+	err := syscall.Gettimeofday(&tv)
+	if err != nil {
+		return time.Now().UnixNano()
+	}
+	return tv.Nano()
+}
+
+func Round(timestamp int64, period int64) int64 {
+	delta := timestamp % period
+	result := timestamp - delta
+	// if delta >= (period >> 1) {
+	// 	result += period
+	// }
+
+	return result
+}
---
+++ fasttime/fasttime_test.go
@@ -0,0 +1,112 @@
+package fasttime
+
+import (
+	"testing"
+	"time"
+)
+
+func BenchmarkMyNow(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Now()
+	}
+}
+
+func BenchmarkTimeNow(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = time.Now()
+	}
+}
+
+func BenchmarkMyRound(b *testing.B) {
+	t := Now()
+	p := int64(time.Hour / time.Second)
+	for i := 0; i < b.N; i++ {
+		_ = Round(t, p)
+	}
+}
+
+func BenchmarkTimeRound(b *testing.B) {
+	t := time.Now()
+	p := time.Hour
+	for i := 0; i < b.N; i++ {
+		_ = t.Round(p)
+	}
+}
+
+func BenchmarkMyNowRound(b *testing.B) {
+	p := int64(time.Hour / time.Second)
+	for i := 0; i < b.N; i++ {
+		_ = Round(Now(), p)
+	}
+}
+
+func BenchmarkTimeNowRound(b *testing.B) {
+	p := time.Hour
+	for i := 0; i < b.N; i++ {
+		_ = time.Now().Round(p)
+	}
+}
+
+func BenchmarkMyDate(b *testing.B) {
+	t := Now()
+	for i := 0; i < b.N; i++ {
+		_, _, _ = ParseDate(t)
+	}
+}
+
+func BenchmarkTimeDate(b *testing.B) {
+	t := time.Now()
+	for i := 0; i < b.N; i++ {
+		_, _, _ = t.Date()
+	}
+}
+
+func BenchmarkMyFormat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		year, month, day := ParseDate(Now())
+		month += 1
+
+		_ = FormatDate(year, month, day)
+	}
+}
+
+func BenchmarkTimeFormat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = time.Now().Format("20060102")
+	}
+}
+
+func BenchmarkMyTotal(b *testing.B) {
+	p := int64(time.Hour / time.Second)
+	for i := 0; i < b.N; i++ {
+		year, month, day := ParseDate(Round(Now(), p))
+		month += 1
+
+		_ = FormatDate(year, month, day)
+	}
+}
+
+func BenchmarkTimeTotal(b *testing.B) {
+	p := time.Hour
+	for i := 0; i < b.N; i++ {
+		_ = time.Now().Round(p).Format("20060102")
+	}
+}
+
+func TestDate(t *testing.T) {
+	t1 := time.Now()
+	t2 := Now()
+
+	y1, m1, d1 := t1.Date()
+	y2, m2, d2 := ParseDate(t2)
+
+	if int64(y1) != y2 {
+		t.Fatal("year:", y1, y2)
+	}
+	if int64(m1)-1 != m2 {
+		t.Fatal("month:", int64(m1)-1, m2)
+	}
+	if int64(d1) != d2 {
+		t.Fatal("day:", d1, d2)
+	}
+}
